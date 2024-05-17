f2bd1566509dfafc02b30853b878f6ff7db063a1
Pavel Grigorenko <GrigorenkoPV@niuitmo.ru>
2024-05-17 13:54:13 +0300

    Docs improvements

--- Include/RTP.h
+++ Include/RTP.h
@@ -13,36 +13,61 @@ class CRTPProcessor : public CRAIDProcessor {
  protected:
   unsigned const p;
 
-  bool IsCorrectable(unsigned int ErasureSetID) override;
+  /// Check if it is possible to correct a given combination of erasures
+  /// If yes, the method should initialize the internal data structures
+  /// and be ready to do the actual erasure correction. This combination of erasures
+  ///  is uniquely identified by ErasureID
+  ///@return true if the specified combination of erasures is correctable
+  bool IsCorrectable(
+      unsigned ErasureSetID  /// identifies the erasure combination. This will not exceed m_Length-1
+      ) override;
 
-  bool DecodeDataSubsymbols(unsigned long long int StripeID,
-                            unsigned int ErasureSetID,
-                            unsigned int SymbolID,
-                            unsigned int SubsymbolID,
-                            unsigned int Subsymbols2Decode,
-                            unsigned char* pDest,
-                            size_t ThreadID) override;
+  /// decode a number of payload subsymbols from a given symbol
+  ///@return true on success
+  bool DecodeDataSubsymbols(
+      unsigned long long StripeID,  /// the stripe to be processed
+      unsigned ErasureSetID,        /// identifies the load balancing offset
+      unsigned SymbolID,            /// the symbol to be processed
+      unsigned SubsymbolID,         /// the first subsymbol to be processed
+      unsigned Subsymbols2Decode,   /// the number of subsymbols within this symbol to be decoded
+      unsigned char*
+          pDest,  /// destination array. Must have size at least Subsymbols2Decode*m_StripeUnitSize
+      size_t ThreadID  /// the ID of the calling thread
+      ) override;
 
-  bool DecodeDataSymbols(unsigned long long int StripeID,
-                         unsigned int ErasureSetID,
-                         unsigned int SymbolID,
-                         unsigned int Symbols2Decode,
-                         unsigned char* pDest,
-                         size_t ThreadID) override;
+  /// decode a number of payload subsymbols from a given symbol
+  ///@return true on success
+  bool DecodeDataSymbols(
+      unsigned long long StripeID,  /// the stripe to be processed
+      unsigned ErasureSetID,        /// identifies the load balancing offset
+      unsigned SymbolID,            /// the first symbol to be processed
+      unsigned Symbols2Decode,      /// the number of subsymbols within this symbol to be decoded
+      unsigned char*
+          pDest,  /// destination array. Must have size at least Subsymbols2Decode*m_StripeUnitSize
+      size_t ThreadID  /// the ID of the calling thread
+      ) override;
 
-  bool EncodeStripe(unsigned long long int StripeID,
-                    unsigned int ErasureSetID,
-                    const unsigned char* pData,
-                    size_t ThreadID) override;
+  /// encode and write the whole CRAIDProcessorstripe
+  ///@return true on success
+  bool EncodeStripe(unsigned long long StripeID,  /// the stripe to be encoded
+                    unsigned ErasureSetID,        /// identifies the load balancing offset
+                    const unsigned char* pData,   /// the data to be envoced
+                    size_t ThreadID               /// the ID of the calling thread
+                    ) override;
 
-  bool UpdateInformationSymbols(unsigned long long int StripeID,
-                                unsigned int ErasureSetID,
-                                unsigned int StripeUnitID,
-                                unsigned int Units2Update,
-                                const unsigned char* pData,
-                                size_t ThreadID) override;
+  /// update some information symbols and the corresponding check symbols
+  ///@return true on success
+  bool UpdateInformationSymbols(unsigned long long StripeID,  /// the stripe to be updated,
+                                unsigned ErasureSetID,  /// identifies the load balancing offset
+                                unsigned StripeUnitID,  /// the first stripe unit to be updated
+                                unsigned Units2Update,  /// the number of units to be updated
+                                const unsigned char* pData,  /// new payload data symbols
+                                size_t ThreadID              /// the ID of the calling thread
+                                ) override;
 
-  bool CheckCodeword(unsigned long long int StripeID,
-                     unsigned int ErasureSetID,
-                     size_t ThreadID) override;
+  /// check if the codeword is consistent
+  bool CheckCodeword(unsigned long long StripeID,  /// the stripe to be checked
+                     unsigned ErasureSetID,        /// identifies the load balancing offset
+                     size_t ThreadID               /// identifies the calling thread
+                     ) override;
 };
--- RAID/RTP.cpp
+++ RAID/RTP.cpp
@@ -24,7 +24,9 @@ bool isPrime(unsigned n) {
 }
 }  // namespace
 
-CRTPProcessor::CRTPProcessor(RTPParams* P)
+/// initialize coding-related parameters
+CRTPProcessor::CRTPProcessor(RTPParams* P  /// the configuration file
+                             )
     : CRAIDProcessor(P->CodeDimension + 3, P->CodeDimension, P, sizeof(*P)),
       p(P->CodeDimension + 1) {
   if (!isPrime(p)) {
@@ -32,53 +34,79 @@ CRTPProcessor::CRTPProcessor(RTPParams* P)
   }
 }
 
-bool CRTPProcessor::IsCorrectable(unsigned int ErasureSetID) {
+/// Check if it is possible to correct a given combination of erasures
+/// If yes, the method should initialize the internal data structures
+/// and be ready to do the actual erasure correction. This combination of erasures
+///  is uniquely identified by ErasureID
+///@return true if the specified combination of erasures is correctable
+bool CRTPProcessor::IsCorrectable(
+    unsigned ErasureSetID  /// identifies the erasure combination. This will not exceed m_Length-1
+) {
   // TODO
   return false;
 }
 
-bool CRTPProcessor::DecodeDataSubsymbols(unsigned long long int StripeID,
-                                         unsigned int ErasureSetID,
-                                         unsigned int SymbolID,
-                                         unsigned int SubsymbolID,
-                                         unsigned int Subsymbols2Decode,
-                                         unsigned char* pDest,
-                                         size_t ThreadID) {
+/// decode a number of payload subsymbols from a given symbol
+///@return true on success
+bool CRTPProcessor::DecodeDataSubsymbols(
+    unsigned long long StripeID,  /// the stripe to be processed
+    unsigned ErasureSetID,        /// identifies the load balancing offset
+    unsigned SymbolID,            /// the symbol to be processed
+    unsigned SubsymbolID,         /// the first subsymbol to be processed
+    unsigned Subsymbols2Decode,   /// the number of subsymbols within this symbol to be decoded
+    unsigned char*
+        pDest,  /// destination array. Must have size at least Subsymbols2Decode*m_StripeUnitSize
+    size_t ThreadID  /// the ID of the calling thread
+) {
   // TODO
   return false;
 }
 
-bool CRTPProcessor::DecodeDataSymbols(unsigned long long int StripeID,
-                                      unsigned int ErasureSetID,
-                                      unsigned int SymbolID,
-                                      unsigned int Symbols2Decode,
-                                      unsigned char* pDest,
-                                      size_t ThreadID) {
+/// decode a number of payload subsymbols from a given symbol
+///@return true on success
+bool CRTPProcessor::DecodeDataSymbols(
+    unsigned long long StripeID,  /// the stripe to be processed
+    unsigned ErasureSetID,        /// identifies the load balancing offset
+    unsigned SymbolID,            /// the first symbol to be processed
+    unsigned Symbols2Decode,      /// the number of subsymbols within this symbol to be decoded
+    unsigned char*
+        pDest,  /// destination array. Must have size at least Subsymbols2Decode*m_StripeUnitSize
+    size_t ThreadID  /// the ID of the calling thread
+) {
   // TODO
   return false;
 }
 
-bool CRTPProcessor::EncodeStripe(unsigned long long int StripeID,
-                                 unsigned int ErasureSetID,
-                                 const unsigned char* pData,
-                                 size_t ThreadID) {
+/// encode and write the whole CRAIDProcessorstripe
+///@return true on success
+bool CRTPProcessor::EncodeStripe(unsigned long long StripeID,  /// the stripe to be encoded
+                                 unsigned ErasureSetID,  /// identifies the load balancing offset
+                                 const unsigned char* pData,  /// the data to be envoced
+                                 size_t ThreadID              /// the ID of the calling thread
+) {
   // TODO
   return false;
 }
 
-bool CRTPProcessor::UpdateInformationSymbols(unsigned long long int StripeID,
-                                             unsigned int ErasureSetID,
-                                             unsigned int StripeUnitID,
-                                             unsigned int Units2Update,
-                                             const unsigned char* pData,
-                                             size_t ThreadID) {
+/// update some information symbols and the corresponding check symbols
+///@return true on success
+bool CRTPProcessor::UpdateInformationSymbols(
+    unsigned long long StripeID,  /// the stripe to be updated,
+    unsigned ErasureSetID,        /// identifies the load balancing offset
+    unsigned StripeUnitID,        /// the first stripe unit to be updated
+    unsigned Units2Update,        /// the number of units to be updated
+    const unsigned char* pData,   /// new payload data symbols
+    size_t ThreadID               /// the ID of the calling thread
+) {
   // TODO
   return false;
 }
 
-bool CRTPProcessor::CheckCodeword(unsigned long long int StripeID,
-                                  unsigned int ErasureSetID,
-                                  size_t ThreadID) {
+/// check if the codeword is consistent
+bool CRTPProcessor::CheckCodeword(unsigned long long StripeID,  /// the stripe to be checked
+                                  unsigned ErasureSetID,  /// identifies the load balancing offset
+                                  size_t ThreadID         /// identifies the calling thread
+) {
   // TODO
   return false;
 }
